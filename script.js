9f1ef771ce51c96850b95c8dd11947e25a5d1dc7
parapat <parapat.patc@gmail.com>
2025-08-30 22:44:00 +0700

    Add function to get human choices.

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ function getComputerChoice(){
     let max_choices = 3;
     return Math.floor(Math.random() * max_choices);
 }
+
+function getHumanChoice(){
+    let choice = prompt("Enter your choices");
+    return choice;
+}
