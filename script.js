a9a21a8618724e577d3c8b2969a3bfa12a8c39ae
parapat <parapat.patc@gmail.com>
2025-08-30 23:00:48 +0700

    Add fn for play using if-else condition.

--- script.js
+++ script.js
@@ -2,12 +2,39 @@ function HelloWorld(){
     return "Hello world!"
 }
 
+// 0 = "Rock"
+// 1 = "Paper"
+// 2 = "Scissors"
+
+
+// This function will returned random choices between 0-2  Note: changes the max_choices var to change the return value.
 function getComputerChoice(){
     let max_choices = 3;
     return Math.floor(Math.random() * max_choices);
 }
 
+// get human choice from prompt input
 function getHumanChoice(){
     let choice = prompt("Enter your choices");
     return choice;
 }
+
+let ComputerScore = 0;
+let HumanScore = 0;
+
+function PlayRound(humanChoice,computerChoice){
+
+    if (humanChoice === computerChoice)     // Check if draw.
+    {
+        return;
+    }
+    else if(( (humanChoice === 0 ) && (computerChoice == 2) ) ||        // Human : Rock    VS Computer : Scissor
+            ( (humanChoice === 1 ) && (computerChoice == 0) ) ||        // Human : Paper   VS Computer : Rock
+            ( (humanChoice === 2 ) && (computerChoice == 1) ) ) {       // Human : Scissor VS Computer : Paper
+            HumanScore += 1;
+    }
+    else{               // Other case computer wins.
+        ComputerScore += 1
+    }
+}
+
