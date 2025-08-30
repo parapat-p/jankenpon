e96b0bedc9aa3650181a161c98c8fcc547f22fea
parapat <parapat.patc@gmail.com>
2025-08-30 23:18:45 +0700

    Fix get Computer to return string & Add game text

--- script.js
+++ script.js
@@ -6,26 +6,27 @@ function HelloWorld(){
 // 1 = "Paper"
 // 2 = "Scissors"
 
+const max_choices = 3;
 
 // This function will returned random choices between 0-2  Note: changes the max_choices var to change the return value.
 function getComputerChoice(){
-    let max_choices = 3;
-    return Math.floor(Math.random() * max_choices);
+    let c_choice = Math.floor(Math.random() * max_choices);
+    if (c_choice === 0){
+        return "rock";
+    }
+    else if (c_choice === 1){
+        return "paper";
+    }
+    else{
+        return "scissor";
+    }
 }
 
 // get human choice from prompt input
 function getHumanChoice(){
     let choice = prompt("Enter your choices");
     choice = choice.toLowerCase();
-    if(choice==="rock"){
-        return 0;
-    }
-    else if(choice==="paper"){
-        return 1;
-    }
-    else{
-        return 2;
-    }
+    return choice;
 }
 
 let ComputerScore = 0;
@@ -35,15 +36,16 @@ function PlayRound(humanChoice,computerChoice){
 
     if (humanChoice === computerChoice)     // Check if draw.
     {
-        return;
+        return "Both select"
     }
-    else if(( (humanChoice === 0 ) && (computerChoice == 2) ) ||        // Human : Rock    VS Computer : Scissor
-            ( (humanChoice === 1 ) && (computerChoice == 0) ) ||        // Human : Paper   VS Computer : Rock
-            ( (humanChoice === 2 ) && (computerChoice == 1) ) ) {       // Human : Scissor VS Computer : Paper
+    else if(( (humanChoice === "rock" ) && (computerChoice == "scissor") ) ||        // Human : Rock    VS Computer : Scissor
+            ( (humanChoice === "paper" ) && (computerChoice == "rock") ) ||        // Human : Paper   VS Computer : Rock
+            ( (humanChoice === "scissor" ) && (computerChoice == "paper") ) ) {       // Human : Scissor VS Computer : Paper
             HumanScore += 1;
+            console.log(`You win nice choice!  ${humanChoice} beat ${computerChoice}`)
     }
     else{               // Other case computer wins.
         ComputerScore += 1
+        console.log(`Bad choices!  ${computerChoice} beat ${computerChoice}`)
     }
 }
-
