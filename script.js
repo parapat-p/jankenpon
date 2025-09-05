ed3f8e651ce352959ffba0ed82cec2353e680e56
parapat-p <parapat.patc@gmail.com>
2025-09-05 19:37:36 +0700

    Add score and modify playRound

--- script.js
+++ script.js
@@ -3,31 +3,63 @@
 // 2 = "Scissors"
 var max_choices = 3;
 var rounds = 5;
+var playerScore = 0;
+var computerScore = 0;
 // This function will returned random choices between 0-2  Note: changes the max_choices var to change the return value.
 function getComputerChoice() {
     var c_choice = Math.floor(Math.random() * max_choices);
     if (c_choice === 0) {
-        return "rock";
+        return "Rock";
     }
     else if (c_choice === 1) {
-        return "paper";
+        return "Paper";
     }
     else {
-        return "scissor";
+        return "Scissor";
     }
+}
+function playRound(playerInput, computerInput) {
+    if (playerInput === computerInput) // Check if draw.
+     {
+        // console.log(`Draw!! Both select ${playerInput}`) ;
+    }
+    else if (((playerInput === "rock") && (computerInput == "scissor")) || // Human : Rock    VS Computer : Scissor
+        ((playerInput === "paper") && (computerInput == "rock")) || // Human : Paper   VS Computer : Rock
+        ((playerInput === "scissor") && (computerInput == "paper"))) // Human : Scissor VS Computer : Paper
+     {
+        playerScore += 1;
+        // console.log(`You win nice choice!  ${playerInput} beat ${computerInput}`)
+    }
+    else { // Other case computer wins.
+        computerScore += 1;
+        // console.log(`Bad choices!  ${computerInput} beat ${playerInput}`)
+    }
+    // console.log(`Current Score $Your score ${playerScore} || Computer score :${computerScore}`)
+    return;
 }
 var gameSection = document.querySelector(".RPS");
-if (gameSection) {
-    var userInput_1 = "";
-    function createBtn(nameBtn) {
-        var button = document.createElement("button");
-        button.textContent = nameBtn;
-        button.addEventListener("click", function () {
-            userInput_1 = nameBtn;
-        });
-        return button;
-    }
-    gameSection.appendChild(createBtn("Scissor"));
-    gameSection.appendChild(createBtn("Rock"));
-    gameSection.appendChild(createBtn("Paper"));
+var scoreBoard = document.createElement("p");
+function updateScore() {
+    scoreBoard.textContent = "Player : ".concat(playerScore, " ||  ").concat(computerScore, " : Computer");
 }
+function createBtn(nameBtn) {
+    var button = document.createElement("button");
+    button.textContent = nameBtn;
+    button.addEventListener("click", function () {
+        playRound(button.textContent, getComputerChoice());
+    });
+    return button;
+}
+function instantiateGame() {
+    if (gameSection) {
+        gameSection.appendChild(createBtn("Scissor"));
+        gameSection.appendChild(createBtn("Rock"));
+        gameSection.appendChild(createBtn("Paper"));
+        updateScore();
+        gameSection.appendChild(scoreBoard);
+    }
+}
+function main() {
+    instantiateGame();
+}
+main();
--- script.ts
+++ script.ts
@@ -4,41 +4,78 @@
 const max_choices:number = 3;
 const rounds:number = 5;
 
+let playerScore:number = 0;
+let computerScore:number = 0;
+
 // This function will returned random choices between 0-2  Note: changes the max_choices var to change the return value.
 function getComputerChoice(){
     let c_choice:number = Math.floor(Math.random() * max_choices);
     if (c_choice === 0){
-        return "rock";
+        return "Rock";
     }
     else if (c_choice === 1){
-        return "paper";
+        return "Paper";
     }
     else{
-        return "scissor";
+        return "Scissor";
     }
 }
 
-
+function playRound(playerInput:string,computerInput:string){
+    if (playerInput === computerInput)     // Check if draw.
+        {
+            // console.log(`Draw!! Both select ${playerInput}`) ;
+        }
+        else if(( (playerInput === "rock" ) && (computerInput == "scissor") ) ||       // Human : Rock    VS Computer : Scissor
+                ( (playerInput === "paper" ) && (computerInput == "rock") ) ||        // Human : Paper   VS Computer : Rock
+                ( (playerInput === "scissor" ) && (computerInput == "paper") ) )      // Human : Scissor VS Computer : Paper
+        {       
+                playerScore += 1;
+                // console.log(`You win nice choice!  ${playerInput} beat ${computerInput}`)
+                
+        }
+        else{               // Other case computer wins.
+            computerScore += 1
+            // console.log(`Bad choices!  ${computerInput} beat ${playerInput}`)
+        }
+        // console.log(`Current Score $Your score ${playerScore} || Computer score :${computerScore}`)
+        return;
+}
 
 const gameSection = document.querySelector(".RPS");
+const scoreBoard = document.createElement("p");
 
-if(gameSection){
-    
-    let userInput:string = "";
-    function createBtn(nameBtn:string){
+
+function updateScore(){
+    scoreBoard.textContent = `Player : ${playerScore} ||  ${computerScore} : Computer`;
+}
+
+function createBtn(nameBtn:string){
     let button = document.createElement("button");
     button.textContent = nameBtn;
     button.addEventListener("click",() => {
-        userInput = nameBtn;
+        playRound(button.textContent,getComputerChoice());
     })
-
-    return button; 
-    }
-
-    gameSection.appendChild(createBtn("Scissor"));
-    gameSection.appendChild(createBtn("Rock"));
-    gameSection.appendChild(createBtn("Paper"));
+    return button;
 }
 
+
+function instantiateGame(){
+    if(gameSection){
+        gameSection.appendChild(createBtn("Scissor"));
+        gameSection.appendChild(createBtn("Rock"));
+        gameSection.appendChild(createBtn("Paper"));
+        updateScore();
+        gameSection.appendChild(scoreBoard);
+    }
+
+}
+
+function main(){
+    instantiateGame();
+
+}
+
+main()
 
 
